68d9997d9bc4ebd3757c30c679b7a315373d5daa
Sniper <sniper_zheng@163.com>
2023-05-02 21:58:16 +0800

    login page

--- my-project/src/components/login/login.vue
+++ my-project/src/components/login/login.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
-    <el-form>
+  <div class="login">
+    <el-form class="login-component">
         <el-form-item label="Username">
             <el-input v-model="username"></el-input>
         </el-form-item>
         <el-form-item label="Password">
             <el-input v-model="password"></el-input>
         </el-form-item>
-        <el-form-item>
-            <el-button type="primary" @click="login">Login</el-button>
-        </el-form-item>
+        <div class="login-btns">
+            <el-form-item>
+                <el-button type="primary" @click="login">Login</el-button>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="register">Register</el-button>
+            </el-form-item>
+        </div>
     </el-form>
   </div>
 </template>
@@ -43,4 +48,15 @@ export default defineComponent({
 })
 </script>
 <style scoped>
+.login-component {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+.login-btns {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
 </style>
--- my-project/src/views/login/index.vue
+++ my-project/src/views/login/index.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
-        <login></login>
+    <div class="content">
+        <img class="login-bg" src="../../assets/login/login-bg.jpeg" mode="aspectFill"/>
+        <div class="login-content">
+            <span class="title-text">JYP</span>
+            <login></login>
+        </div>
     </div>
 </template>
 <script>
 import login from '@/components/login/login.vue'
 import { defineComponent } from 'vue'
 export default defineComponent({
-    name: 'login-page',
     components: {
         login,
     },
@@ -34,5 +37,40 @@ export default defineComponent({
 })
 </script>
 <style scoped>
-
+.title-text {
+    font-size: 24px;
+    font-weight: bold;
+    color: #333;
+    margin-bottom: 20px;
+    /* background-color: green; */
+    display: block;
+    text-align: center;
+}
+.content {
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    /* display: flex; */
+}
+.login-bg {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    /* opacity: 0.5; */
+    left: 0px;
+    top: 0px;
+    background-size: cover;
+}
+.login-content {
+    width: 360px;
+    /* height: 300px; */
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    padding: 20px 60px;
+    border-radius: 8px;
+}
 </style>
