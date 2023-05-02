09b33cdd1078d4567db5160119f97bf4332673b4
Sniper <sniper_zheng@163.com>
2023-05-02 21:06:34 +0800

    elementUI 按需引入

--- my-project/src/components/login/login.vue
+++ my-project/src/components/login/login.vue
@@ -1,21 +1,20 @@
 <template>
-    <div>
-        <el-form>
-            <el-form-item label="Username">
-                <el-input v-model="username" />
-            </el-form-item>
-            <el-form-item label="Password">
-                <el-input v-model="password" />
-            </el-form-item>
-            <el-form-item>
-                <el-button @click="login">Login</el-button>
-            </el-form-item>
-        </el-form>
-    </div>
+  <div>
+    <el-form>
+        <el-form-item label="Username">
+            <el-input v-model="username"></el-input>
+        </el-form-item>
+        <el-form-item label="Password">
+            <el-input v-model="password"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="login">Login</el-button>
+        </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
 import { defineComponent } from 'vue'
-import { ElForm, ElFormItem, ElInput, ElButton } from 'element-plus'
 export default defineComponent({
     name: 'login-component',
     components: {
