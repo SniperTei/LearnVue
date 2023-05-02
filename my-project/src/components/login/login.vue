f6642ae72a7defa536fea95139ddbf2e851ccca7
Sniper <sniper_zheng@163.com>
2023-05-02 20:48:36 +0800

    login

---
+++ my-project/src/components/login/login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        wangdefa
+        <!-- <el-form>
+            <el-form-item label="Username">
+                <el-input v-model="username" />
+            </el-form-item>
+            <el-form-item label="Password">
+                <el-input v-model="password" />
+            </el-form-item>
+            <el-form-item>
+                <el-button @click="login">Login</el-button>
+            </el-form-item>
+        </el-form> -->
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'login-component',
+    components: {
+
+    },
+    setup() {
+        return {}
+    },
+    mounted() {
+        console.log('login-component mounted')
+    },
+    data() {
+        return {
+            username: '',
+            password: '',
+        }
+    },
+    methods: {
+        login() {
+            this.$store.dispatch('login', {
+                username: this.username,
+                password: this.password,
+            })
+        },
+    },
+})
+</script>
+<style scoped>
+</style>
---
+++ my-project/src/views/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <login></login>
+    </div>
+</template>
+<script>
+import login from '@/components/login/login.vue'
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'login-page',
+    components: {
+        login,
+    },
+    mounted() {
+        console.log('login-page mounted')
+    },
+    setup() {
+        return {}
+    },
+    data() {
+        return {
+            // username: '',
+            // password: '',
+        }
+    },
+    methods: {
+        login() {
+            // this.$store.dispatch('login', {
+            //     username: this.username,
+            //     password: this.password,
+            // })
+        },
+    },
+})
+</script>
+<style scoped>
+
+</style>
