5968fea7dc8abe613022e4e0d323bc67b6211845
Sniper <sniper_zheng@163.com>
2024-03-10 15:17:53 +0800

    good item ok

--- learn-rabbit-shopping/src/directives/index.js
+++ learn-rabbit-shopping/src/directives/index.js
@@ -10,15 +10,18 @@ export const lazyPlugin = {
       mounted(el, binding) {
         useIntersectionObserver(el,
           ([{ isIntersecting }], observerElement) => {
+            // console.log("isIntersecting: ", isIntersecting)
             if (isIntersecting) { // 进入了视口区域
               const img = new Image()
               img.src = binding.value
-              img.onload = () => {
+              img.onload = () => { // 当图片加载完成时，触发 onload 事件
+                // 将目标元素（通常是 <img> 标签）的 src 属性设置为新图片的 src，从而实现懒加载图片的效果
                 el.src = img.src
                 observerElement.disconnect()
               }
             }
           },
+          // 通过设置 threshold: 0.5，表示当目标元素的一半进入视口时，就会触发回调函数。可以根据实际需求调整阈值来控制触发懒加载的时机
           { threshold: 0.5 }
         )
       }
--- learn-rabbit-shopping/src/views/home/components/homeNew.vue
+++ learn-rabbit-shopping/src/views/home/components/homeNew.vue
@@ -6,28 +6,31 @@ import { onMounted, ref } from 'vue';
 
 const newList = ref([])
 const getNewList = async () => {
-	const res = await findNewAPI()
-	console.log('res : ', res)
-	newList.value = res.data.result
+    const res = await findNewAPI()
+    console.log('res : ', res)
+    newList.value = res.data.result
 }
 onMounted(() => {
-	getNewList()
+    getNewList()
 })
 </script>
  
 <template>
-	<HomePanel title="新鲜好物" subTitle="新鲜出炉 品质靠谱">
-		<ul class="goods-list">
-		<li v-for="item in newList" :key="item.id">
-			<RouterLink :to="`/detail/${item.id}`">
-			<!-- <img :src="item.picture" alt="" /> -->
-			<img v-img-lazy="item.picture" alt="" />
-			<p class="name">{{ item.name }}</p>
-			<p class="price">&yen;{{ item.price }}</p>
-			</RouterLink>
-		</li>
-		</ul>
-	</HomePanel>
+    <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+        <ul class="goods-list">
+        <li v-for="item in newList" :key="item.id">
+            <RouterLink :to="`/detail/${item.id}`">
+            <!-- <img :src="item.picture" alt="" /> -->
+            <img v-img-lazy="item.picture" alt="" />
+            <p class="name">{{ item.name }}</p>
+            <p class="price">&yen;{{ item.price }}</p>
+            </RouterLink>
+        </li>
+        </ul>
+        <!-- <div v-for="item in newList" :key="item.id">
+            <GoodsItem :goods="item"></GoodsItem>
+        </div> -->
+    </HomePanel>
 </template>
 
 <style scoped lang='scss'>
@@ -38,34 +41,34 @@ onMounted(() => {
   height: 406px;
 
   li {
-	width: 306px;
-	height: 406px;
+    width: 306px;
+    height: 406px;
 
-	background: #f0f9f4;
-	transition: all .5s;
+    background: #f0f9f4;
+    transition: all .5s;
 
-	&:hover {
-	  transform: translate3d(0, -3px, 0);
-	  box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-	}
+    &:hover {
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
 
-	img {
-	  width: 306px;
-	  height: 306px;
-	}
+    img {
+      width: 306px;
+      height: 306px;
+    }
 
-	p {
-	  font-size: 22px;
-	  padding-top: 12px;
-	  text-align: center;
-	  text-overflow: ellipsis;
-	  overflow: hidden;
-	  white-space: nowrap;
-	}
+    p {
+      font-size: 22px;
+      padding-top: 12px;
+      text-align: center;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
 
-	.price {
-	  color: $priceColor;
-	}
+    .price {
+      color: $priceColor;
+    }
   }
 }
 </style>
