1ce664ae85bb3f995c368058ef422407a5758720
Sniper <sniper_zheng@163.com>
2024-03-16 14:01:05 +0800

    tab切换

--- learn-rabbit-shopping/src/views/subCategory/index.vue
+++ learn-rabbit-shopping/src/views/subCategory/index.vue
@@ -33,6 +33,12 @@ onMounted(() => {
   getGoodsList();
 });
 
+// tab切换回调
+const tabChange = () => {
+  reqData.value.page = 1;
+  getGoodsList();
+};
+
 </script>
 
 <template>
@@ -46,7 +52,7 @@ onMounted(() => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
