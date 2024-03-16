b6f1500d78d184a3d3aff7f94dd95cebaad16944
Sniper <sniper_zheng@163.com>
2024-03-16 21:06:39 +0800

    热榜 参数

--- learn-rabbit-shopping/src/views/detail/components/detailHot.vue
+++ learn-rabbit-shopping/src/views/detail/components/detailHot.vue
@@ -3,11 +3,29 @@
 import { onMounted, ref } from 'vue'
 import { getHotGoodsAPI } from '@/apis/detail'
 import { useRoute } from 'vue-router'
+import { computed, defineProps } from 'vue'
+
+const props = defineProps({
+  hotType: {
+    type: Number,
+    default: 1
+  }
+})
+
+// 适配title 1- 24小时热榜 2- 周日榜单 3- 周榜单
+const TYPEMAP = {
+  1: '24小时热榜',
+  2: '周日榜单',
+  3: '周榜单'
+}
+
+const title = computed(() => TYPEMAP[props.hotType])
+
 const hotList = ref([])
 const getHotList = async () => {
   const res = await getHotGoodsAPI({
     id: useRoute().params.id,
-    type: 1
+    type: props.hotType
   })
   hotList.value = res.data.result
 
@@ -20,7 +38,7 @@ onMounted(() => {
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
       <img :src="item.picture" alt="" />
