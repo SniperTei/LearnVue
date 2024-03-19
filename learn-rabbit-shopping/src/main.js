1b6fda2ee9201fc51d99660f7050f3ca4d48ee2f
Sniper <sniper_zheng@163.com>
2024-03-19 22:37:08 +0800

    pinia 持久化

--- learn-rabbit-shopping/src/main.js
+++ learn-rabbit-shopping/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { lazyPlugin } from '@/directives/index.js'
 import { componentPlugin } from '@/components/index.js'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -10,8 +11,10 @@ import router from './router'
 import '@/styles/common.scss'
 
 const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 // Use the lazy plugin
 app.use(lazyPlugin);
--- learn-rabbit-shopping/src/stores/user.js
+++ learn-rabbit-shopping/src/stores/user.js
@@ -1,20 +1,21 @@
 // 用户数据相关的store
 import { defineStore } from "pinia";
 import { ref } from "vue";
-import { loginAPI } from '@/apis/user'
+// import { loginAPI } from '@/apis/user'
 
 export const useUserStore = defineStore('user', () => {
   // 1. 定义管理用户数据
   const userInfo = ref({})
   // 2. 定义获取接口数据的aciton函数
-  const getUserInfo = async (account, password) => {
-    const res = await loginAPI({account, password})
-    console.log('res in user store', res);
+  const getUserInfo = async ({account, password}) => {
+    // const res = await loginAPI({account, password})
+    console.log('res in user store', '');
     // userInfo.value = res.result
     userInfo.value = {
-      account: 'admin',
+      account: account,
       mobile: '13800138000',
-      token: 'thisisatoken'
+      token: 'thisisatoken',
+      password: password
     }
   }
   // 3. 以对象的格式把state和action都return出去
@@ -22,4 +23,6 @@ export const useUserStore = defineStore('user', () => {
     userInfo,
     getUserInfo
   }
+}, {
+  persist: true
 });
