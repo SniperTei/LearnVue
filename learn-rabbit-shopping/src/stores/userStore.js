41fa330a8166fdd79ee1826ddbd0bdfd5829b49e
Sniper <sniper_zheng@163.com>
2024-03-27 12:42:37 +0800

    clear cart when logout

--- learn-rabbit-shopping/src/stores/userStore.js
+++ learn-rabbit-shopping/src/stores/userStore.js
@@ -2,10 +2,12 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import { loginAPI } from '@/apis/user'
+import { useCartStore } from './cartStore'
 
 export const useUserStore = defineStore('user', () => {
   // 1. 定义管理用户数据
   const userInfo = ref({})
+  const cartStore = useCartStore()
   // 2. 定义获取接口数据的aciton函数
   const getUserInfo = async ({account, password}) => {
     const res = await loginAPI({account, password})
@@ -16,6 +18,8 @@ export const useUserStore = defineStore('user', () => {
   // 3. 定义清除用户数据的action函数
   const clearUserInfo = () => {
     userInfo.value = {}
+    // 清除购物车数据
+    cartStore.clearCart()
   }
   // 3. 以对象的格式把state和action都return出去
   return {
