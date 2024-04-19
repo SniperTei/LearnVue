4c570e315e71040052038188a9f0c9b694cabab1
Sniper <sniper_zheng@163.com>
2024-04-19 14:41:57 +0800

    带tooltip的下拉

--- vue-elementui/src/App.vue
+++ vue-elementui/src/App.vue
@@ -2,17 +2,39 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PersonalZoneSelect :options="options" placement="top" effect="light" v-model="selectedValue"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PersonalZoneSelect from './components/personalZoneSelect.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    PersonalZoneSelect
+  },
+  data() {
+    return {
+      options: [
+        {
+          value: '1',
+          label: 'Option 1'
+        },
+        {
+          value: '2',
+          label: 'Option 2'
+        },
+        {
+          value: '3',
+          label: 'Option 3'
+        }
+      ],
+      selectedValue: '2'
+    }
+  },
 }
 </script>
 
---
+++ vue-elementui/src/components/personalZoneSelect.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <!-- Your template code goes here -->
+    <el-tooltip :content="tooltipValue" size="small" :placement="placement" :effect="effect">
+      <el-select v-model="selectValue" placeholder="请选择">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        ></el-option>
+      </el-select>
+      <!-- <template slot="content">
+        <div>
+          <span>这是一段内容</span>
+          <el-button size="mini" type="text">按钮</el-button>
+        </div>
+      </template> -->
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonalZoneSelect',
+  props: {
+    // Your component props go here
+    options: {
+      type: Array,
+      required: true,
+    },
+    placement: {
+      type: String,
+      default: 'top',
+    },
+    effect: {
+      type: String,
+      default: 'light',
+    },
+    value: {
+      type: String,
+      default: '',
+    }
+  },
+  data() {
+    return {
+      selectValue: this.value,
+    };
+  },
+  computed: {
+    tooltipValue() {
+      // 如果selectValue有值
+      if (this.selectValue) {
+        // 获取当前选中的option
+        const selectedOption = this.options.find(option => option.value === this.selectValue);
+        // 返回选中的option的label
+        return selectedOption ? selectedOption.label : '';
+      }
+      return '';
+    }
+  },
+  methods: {
+    // Your methods go here
+  },
+  mounted() {
+    // Code to run when the component is mounted goes here
+  },
+};
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
