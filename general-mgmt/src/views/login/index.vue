245fa047849f78c9db76d949f3c3bf40edf0c28d
Sniper <sniper_zheng@163.com>
2025-01-01 21:36:18 +0800

    优化登录UI

--- general-mgmt/src/views/login/index.vue
+++ general-mgmt/src/views/login/index.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { ElMessage, ElButton, ElForm, ElFormItem, ElInput } from 'element-plus'
+import { User, Lock } from '@element-plus/icons-vue'
 // 引入md5
 import md5 from 'js-md5'
 // userStore
@@ -22,32 +23,34 @@ const login = async () => {
     ElMessage.error('登录失败: ' + errMsg)
   })
 }
-// const login = async () => {
-//   console.log(username.value, password.value)
-//   // 登录逻辑
-//   await userStore.getUserInfo({ username: username.value, password: password.value})
-//   // await userStore.getUserInfo({ username.value, password.value })
-//   ElMessage.success('登录成功')
-//   router.replace('/')
-// }
 </script>
 
 <template>
   <div class="login-container">
     <el-form class="login-form">
-      <h3>YOLO系统</h3>
-      <div>
-        <el-form-item >
-          <el-input v-model="username"></el-input>
+      <h3 class="title">YOLO系统</h3>
+      <div class="form-item">
+        <el-form-item>
+          <el-input 
+            v-model="username"
+            placeholder="请输入用户名"
+            :prefix-icon="User"
+          ></el-input>
         </el-form-item>
       </div>
-      <div>
-        <el-form-item >
-          <el-input type="password" v-model="password"></el-input>
+      <div class="form-item">
+        <el-form-item>
+          <el-input 
+            type="password" 
+            v-model="password"
+            placeholder="请输入密码"
+            :prefix-icon="Lock"
+            @keyup.enter="login"
+          ></el-input>
         </el-form-item>
       </div>
-      <div>
-        <el-button type="primary" @click="login">Login</el-button>
+      <div class="form-item">
+        <el-button type="primary" @click="login" class="login-button">登录</el-button>
       </div>
     </el-form>
   </div>
@@ -56,27 +59,64 @@ const login = async () => {
 <style lang="scss" scoped>
 .login-container {
   background-color: #2d3a4b;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  
   .login-form {
-    width: 300px;
-    padding: 20px;
-    border-radius: 5px;
+    width: 400px;
+    padding: 35px;
+    border-radius: 8px;
     background-color: #304156;
-    h3 {
-      color: white;
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+    
+    .title {
+      color: #fff;
       text-align: center;
+      font-size: 26px;
+      font-weight: bold;
+      margin-bottom: 35px;
     }
-    // 按钮
-    .el-button {
+
+    .form-item {
+      margin-bottom: 25px;
+
+      &:last-child {
+        margin-bottom: 0;
+      }
+
+      :deep(.el-input) {
+        .el-input__wrapper {
+          background-color: rgba(255, 255, 255, 0.1);
+          border: 1px solid rgba(255, 255, 255, 0.1);
+          box-shadow: none;
+          
+          &:hover, &:focus {
+            border-color: #409EFF;
+          }
+        }
+
+        input {
+          color: #fff;
+          height: 40px;
+          
+          &::placeholder {
+            color: rgba(255, 255, 255, 0.7);
+          }
+        }
+
+        .el-input__prefix-inner {
+          color: rgba(255, 255, 255, 0.7);
+        }
+      }
+    }
+
+    .login-button {
       width: 100%;
+      height: 40px;
+      font-size: 16px;
     }
   }
-}
-
-h1 {
-  color: white;
 }
 </style>
