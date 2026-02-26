3039d70e12297c901b1c931da02876c4ea8039b4
Sniper <sniper_zheng@163.com>
2026-02-26 13:56:31 +0800

    delete many project

--- learn-rabbit-shopping/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import ImageView from './imageView/index.vue';
-import Sku from './XtxSku/index.vue';
-
-export const componentPlugin = {
-  install(app) {
-    app.component('XtxImageView', ImageView);
-    app.component('XtxSku', Sku);
-  }
-};
--- yimei-demo/src/config/version.js
+++
@@ -1,9 +0,0 @@
-export const APP_VERSION = 'v1.0.0'
-export const BEIAN_NO = '备案号'
-
-// 如果需要更多版本相关的信息，可以在这里添加
-export const VERSION_INFO = {
-  version: APP_VERSION,
-  buildDate: '2025-11-05',
-  environment: import.meta.env.MODE
-}
